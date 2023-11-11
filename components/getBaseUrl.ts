90676fafc10a25f5deef78261914d74318fb6262
Noble Osinachi <nobleosinachi98@gmail.com>
2023-11-11 02:26:00 +0100

    Fixed build conflicts in last deployment

--- components/getBaseUrl.ts
+++ components/getBaseUrl.ts
@@ -1,11 +1,12 @@
 import { headers } from "next/headers";
 
-export function getBaseUrl() {
+const getBaseUrl = () => {
   const headersList = headers();
 
   const baseUrl = process.env.VERCEL_URL
     ? "https://" + process.env.VERCEL_URL
     : "http://" + headersList.get("host");
-
   return baseUrl;
-}
+};
+
+export default getBaseUrl;
